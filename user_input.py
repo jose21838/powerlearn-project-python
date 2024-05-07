e3cdbd1a24e0f44c5d97bf8897377758d9121df9
jose21838 <josephochieng922@gmail.com>
2024-05-07 11:33:17 +0300

    powerlearn-project-python

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#ask user for their names
+name=input("please enter yur name")
+#ask user for their names
+age=input("please enter your age")
+#ask user for their location
+location=input("please enter your location of stay")
+#print users personal message
+print("Hello ",name, "you are" ,age, "years old and live in" ,location)
